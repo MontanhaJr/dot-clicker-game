bf942d7fa820e6de1c735e34757ac0fc29b4635d
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2026-03-15 21:38:49 -0300

    feat: add admob

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,16 +6,14 @@ plugins {
 
 android {
     namespace = "com.montanhajr.pointgame"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.montanhajr.pointgame"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -50,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.google.play.services.ads)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/montanhajr/pointgame/MainActivity.kt
+++ app/src/main/java/com/montanhajr/pointgame/MainActivity.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import com.google.android.gms.ads.MobileAds
 import com.montanhajr.pointgame.ui.screens.GameModeScreen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Inicializa o AdMob em background
+        val backgroundScope = CoroutineScope(Dispatchers.IO)
+        backgroundScope.launch {
+            MobileAds.initialize(this@MainActivity) {}
+        }
+
         setContent {
             MaterialTheme {
                 GameModeScreen()
