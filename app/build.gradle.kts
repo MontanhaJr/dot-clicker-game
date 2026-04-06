4e213d3acc7833d1020544079942f1f0048af20f
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2026-04-05 23:23:39 -0400

    feat: add play serivces games

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.montanhajr.pointgame"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.3.2"
+        versionCode = 10
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
