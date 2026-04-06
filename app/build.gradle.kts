f78aa21c96d9a7a5f802f75f9bb59f3afff4cae6
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2026-04-06 01:29:07 -0400

    feat: update achievement id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.montanhajr.pointgame"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.4.0"
+        versionCode = 11
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
